ec7865a4a2892053906ad259a743c58086a2f3d9
FabienFattore <aiolos79xbox@gmail.com>
2023-06-27 11:15:11 +0200

    style(navbar): style of navbar

--- src/App/Views/head.php
+++ src/App/Views/head.php
@@ -2,30 +2,30 @@
 <html lang="en">
 
 <head>
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/php-crud/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="/Bonnefete/dist/output.css">
+  <link rel="stylesheet" href="/Bonnefete/src/public/css/style.css">
+  <title>BONNEFETE</title>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="../char/index">CRUD</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse show" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="../char/index">Home</a>
-        <?php if (isset($_SESSION['user'])) : ?>
-          <a class="nav-item nav-link" href="../user/logout">Se deconnecter</a>
-          <a class="nav-item nav-link" href="../char/create">Ajoutez un personnage</a>
+  <nav class="navbar">
+    <a class="navbar-brand" href="../char/index">BONNEFETE</a>
 
-        <?php else : ?>
-          <a class="nav-item nav-link" href="../user/login">Se connecter</a>
-          <a class="nav-item nav-link" href="../user/register">S'enregistrer</a>
-        <?php endif; ?>
+    <?php if (isset($_SESSION['user'])) : ?>
+      <div class="nav-user">
+        <a class="nav-user-profile">Profil</a>
+        <a class="nav-quit" href="../user/logout">Se déconnecter</a>
+        <div class="nav-user-id"><a href="">
+            <img class="nav-img-user" src="/Bonnefete/src/public/assets/images/photo-avatar-profil.png" alt=""></a>
+          <a class="nav-user-name">Utilisateur</a>
+        </div>
       </div>
-    </div>
+
+      <?php else : ?><?php endif; ?>
+
   </nav>
+</body>
