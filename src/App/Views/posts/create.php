d221bf62b063da1f5bf8a680a93d993cb3de5015
FabienFattore <aiolos79xbox@gmail.com>
2023-06-28 11:16:07 +0200

    style(post): new post page

--- src/App/Views/posts/create.php
+++ src/App/Views/posts/create.php
@@ -1,26 +1,15 @@
 <?php require_once '../Bonnefete/src/App/Views/head.php'; ?>
 
-<div class="post-create">
-  <form action="../post/create" method="post">
-    <div>
-      <label for="title">Titre</label>
-      <input type="text" name="title" id="title">
-    </div>
-    <div>
-      <label for="content">Contenu</label>
-      <textarea name="content" id="content" cols="30" rows="10"></textarea>
-    </div>
-    <<<<<<<<< Temporary merge branch 1 <button>Publier</button>
-  </form>
-  =========
-  <div>
-    <label for="date">Date</label>
-    <input type="date" name="date" id="date">
 
-  </div>
-  <button class="post">Publier</button>
+  <form class="post-create" action="../post/create" method="post">
+    <label for="title">Titre</label>
+    <input type="text" name="title" id="title">
+    <label class="content" for="content">Contenu</label>
+    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    <button id="post" >Publier</button>
+    <button class="post">Publier</button>
   </form>
-</div>
->>>>>>>>> Temporary merge branch 2
+
+
 
 <?php require_once '../Bonnefete/src/App/Views/foot.php'; ?>
