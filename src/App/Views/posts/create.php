990d071b883278ee946139aeaccb42d2d44d4ddb
FabienFattore <aiolos79xbox@gmail.com>
2023-06-28 11:56:27 +0200

    style(form): form new post

--- src/App/Views/posts/create.php
+++ src/App/Views/posts/create.php
@@ -3,9 +3,9 @@
 
   <form class="post-create" action="../post/create" method="post">
     <label for="title">Titre</label>
-    <input type="text" name="title" id="title">
+    <input type="text" name="title" id="title" required>
     <label class="content" for="content">Contenu</label>
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    <textarea name="content" id="content" cols="30" rows="10" maxlength="200" required></textarea>
     <button id="post" >Publier</button>
     <button class="post">Publier</button>
   </form>
