f8e56249b35a4a3fdc41c671f5fbaca9dd8bbe93
FabienFattore <aiolos79xbox@gmail.com>
2023-06-28 10:29:14 +0200

    fix(fix): fix btn

--- src/App/Views/posts/create.php
+++ src/App/Views/posts/create.php
@@ -1,25 +1,26 @@
 <?php require_once '../Bonnefete/src/App/Views/head.php'; ?>
 
-<div class="post-create"><form action="../post/create" method="post">
-  <div>
-    <label for="title">Titre</label>
-    <input type="text" name="title" id="title">
-  </div>
-  <div>
-    <label for="content">Contenu</label>
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
-  </div>
-<<<<<<<<< Temporary merge branch 1
-  <button>Publier</button>
-</form>
-=========
+<div class="post-create">
+  <form action="../post/create" method="post">
+    <div>
+      <label for="title">Titre</label>
+      <input type="text" name="title" id="title">
+    </div>
+    <div>
+      <label for="content">Contenu</label>
+      <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    </div>
+    <<<<<<<<< Temporary merge branch 1 <button>Publier</button>
+  </form>
+  =========
   <div>
     <label for="date">Date</label>
     <input type="date" name="date" id="date">
 
   </div>
   <button class="post">Publier</button>
-</form></div>
+  </form>
+</div>
 >>>>>>>>> Temporary merge branch 2
 
-  <?php require_once '../Bonnefete/src/App/Views/foot.php'; ?>
+<?php require_once '../Bonnefete/src/App/Views/foot.php'; ?>
