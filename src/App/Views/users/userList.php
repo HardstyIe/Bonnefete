b0084c108dfca85e869900bba044232240bacec7
FabienFattore <aiolos79xbox@gmail.com>
2023-07-06 10:50:20 +0200

    fix(fix): background img fix

--- src/App/Views/users/userList.php
+++ src/App/Views/users/userList.php
@@ -1,7 +1,7 @@
 <?php require_once '../Bonnefete/src/App/Views/head.php'; ?>
 
 <!-- fait moi un tableau tailwind qui repertorie tous mes utilisateur , avec l'option de modifier ou supprimer l'utilisateur -->
-<?php if ($_SESSION['user']['Role_Name'] == "Administrateur" || "SuperAdministrateur") : ?>
+<?php if ($_SESSION['users']['rolename'] == "Administrateur" || "SuperAdministrateur") : ?>
   <main class="user-list">
     <h1 class="text-4xl font-bold">Liste des utilisateurs</h1>
     <table class="table-auto">
@@ -17,13 +17,13 @@
       <tbody>
         <?php foreach ($users as $user) : ?>
           <tr>
-            <td class="px-4 py-2 border"><?= $user['User_Surname'] ?></td>
-            <td class="px-4 py-2 border"><?= $user['User_Name'] ?></td>
-            <td class="px-4 py-2 border"><?= $user['User_Email'] ?></td>
-            <td class="px-4 py-2 border"><?= $user['Nb_Post'] ?></td>
+            <td class="px-4 py-2 border"><?= $user['surname'] ?></td>
+            <td class="px-4 py-2 border"><?= $user['name'] ?></td>
+            <td class="px-4 py-2 border"><?= $user['email'] ?></td>
+            <td class="px-4 py-2 border"><?= $user['nb_posts'] ?></td>
             <td class="px-4 py-2 border">
-              <a href="/Bonnefete/user/update/<?= $user['User_Id'] ?>" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Modifier</a>
-              <a href="/Bonnefete/user/delete/<?= $user['User_Id'] ?>" class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700">Supprimer</a>
+              <a href="/Bonnefete/user/update/<?= $user['id'] ?>" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Modifier</a>
+              <a href="/Bonnefete/user/delete/<?= $user['id'] ?>" class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700">Supprimer</a>
             </td>
           </tr>
         <?php endforeach; ?>
